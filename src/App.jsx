bb998904496cf29c18b51127c7b2b4c872adab01
theflawlesserror <johann.t.philip@gmail.com>
2026-04-04 21:00:21 +0530

    upd: silly stuff

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ return (
         )}
       </div>
       <div className="mt-auto pt-8 z-10 text-center">
-        <p className="text-zinc-500 text-xs sm:text-sm font-medium tracking-widest opacity-80">
+        <p className="text-zinc-500 text-xs sm:text-sm font-medium tracking-widest">
           <span className="text-red-500">xoxo, </span>
           <span className="text-white">Johann</span>
         </p>
